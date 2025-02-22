5f76d3bb5388404746a149886babad5b70625f0a
silksofthesoul <silksofthesoul@yandex.ru>
2025-02-22 21:59:52 +0300

    feat: добавлен класс Route

---
+++ classes/Route.js
@@ -0,0 +1,67 @@
+'use strict';
+const { isAsync, isPromise } = require('../utils');
+
+class Route {
+  static #routes = [];
+
+  url = '/';
+  method = 'get';
+  template = 'index';
+  data = {};
+  navigation = {};
+
+  constructor(url, method = get, params = {}) {
+    const { template, data = null, navigation } = params;
+    this.url = url;
+    if (method) this.method = method;
+    if (template) this.template = template;
+    if (data) this.data = data;
+    this.setupNavigation(navigation);
+  }
+
+  setupNavigation(navigation) {
+    const { type, text, hidden } = navigation;
+    Route.#routes.push({
+      url: this.url,
+      method: this.method,
+      text,
+      type,
+      hidden,
+    });
+    return this;
+  }
+
+  static getRoutes() {
+    return Route.#routes.filter(({ hidden }) => !hidden);
+  }
+
+  static makeNavigation(type, text, hidden = false) {
+    return { type, text, hidden };
+  }
+
+  setData(data) {
+    this.data = { ...this.data, ...data };
+    return this;
+  }
+
+  async listen(app, params = {}) {
+    const { fn = null, redirect = null, outerRedirect = false } = params;
+    app[this.method](this.url, async (req, res, next) => {
+      this.data.nav = Route.getRoutes();
+      if (fn) {
+        let resFn = {};
+        if (isAsync(fn)) resFn = await fn(this, req, res);
+        else if (isPromise(fn)) resFn = await fn(this, req, res);
+        else resFn = fn(this, req, res);
+        const { data } = resFn || {};
+        this.data = { ...this.data, ...data };
+        if (outerRedirect) return null;
+      }
+      if (redirect) res.redirect(redirect);
+      else res.render(this.template, this.data);
+    });
+    return this;
+  }
+};
+
+module.exports.Route = Route;
