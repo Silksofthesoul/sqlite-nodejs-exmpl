73f08b30649893bcc64892ca084d0629eb318b24
silksofthesoul <silksofthesoul@yandex.ru>
2025-02-27 22:19:53 +0300

    chore: cli/init-users.js was changed

--- cli/init-users.js
+++ cli/init-users.js
@@ -6,13 +6,13 @@ const cli = async _ => {
 
   const db = new Users({ filename });
   await db.init();
-  // await db.create();
-  // await db.initGroups();
+  await db.create();
+  await db.initGroups();
   // await db.addUser({ name: 'admin', email: 'admin@admin.com', password: '123456', groupID: 0 });
   // await db.addUser({ name: 'admin2', email: 'admin2@admin.com', password: '123456', groupID: 0 });
   // await db.addUser({ name: 'test', email: 'test@test.com', password: '123456', groupID: 10 });
-  const user = await db.getUserByAuth({ email: 'admin@admin.com', password: '123456' });
-  console.log(user, '<<< LOG');
+  // const user = await db.getUserByAuth({ email: 'admin@admin.com', password: '123456' });
+  // console.log(user, '<<< LOG');
   db.close();
 };
 cli();
